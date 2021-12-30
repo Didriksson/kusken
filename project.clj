034a07b9b8ba918772a6665d8e138c098096bbeb
Mattias Didriksson <mattias.didriksson@gmail.com>
2021-12-30 23:15:51 +0100

    Min lein version.

--- project.clj
+++ project.clj
@@ -3,11 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :mirrors {#"central" {:name "central"
-                       :url "https://repo1.maven.org/maven2"}
-            #"clojars" {:name "clojars"
-                        :url "https://repo.clojars.org"
-                        :repo-manager true}}
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [reaver "0.1.3"]
                  [clj-http "3.12.3"]
