6b2ae63e6a6da6b1f5927151e5f0ced69a9d3e75
Mattias Didriksson <mattias.didriksson@gmail.com>
2021-12-30 23:13:01 +0100

    Mitt sista halmstrå.

--- project.clj
+++ project.clj
@@ -4,7 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :mirrors {#"central" {:name "central"
-                       :url "https://repo1.maven.org/maven2"}}
+                       :url "https://repo1.maven.org/maven2"}
+            #"clojars" {:name "clojars"
+                        :url "https://repo.clojars.org"
+                        :repo-manager true}}
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [reaver "0.1.3"]
                  [clj-http "3.12.3"]
